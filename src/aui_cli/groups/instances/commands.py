78a5cfb39584ec268ac8e93459cd5686d12136e0
Vladimir Ritz Bossicard <vlritz@microsoft.com>
2021-10-13 21:26:25 +0200

    Add missing file

--- src/aui_cli/groups/instances/commands.py
+++ src/aui_cli/groups/instances/commands.py
@@ -1,11 +1,27 @@
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+
+from aui_cli.common.services import get_client, get_custom_headers
 
 
 def list_instances():
     """
     Lists all instances.
     """
+    client = get_client()
 
-    print('list instances')
+    instances = client.getallinstances(custom_headers=get_custom_headers())
+    return instances
+
+
+def show_instance(instance_id):
+    """
+    Show an instance
+    """
+
+    client = get_client()
+
+    instance = client.getinstancemetadata(instance_id, custom_headers=get_custom_headers())
+    return instance
