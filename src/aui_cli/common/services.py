ff20a0d57d86a5fb0ee243f2a94cc351a4250024
Vladimir Ritz Bossicard <vlritz@microsoft.com>
2021-10-18 09:36:40 +0200

    adding documentation

--- src/aui_cli/common/services.py
+++ src/aui_cli/common/services.py
@@ -9,12 +9,20 @@ from .config import GLOBAL_CONFIG
 
 
 def get_client():
+    """
+    Returns an authenticated CustomerInsights client.
+    """
+
     auth_token = GLOBAL_CONFIG.get("authentication", "token")
     auth = BasicTokenAuthentication({'access_token': auth_token})
     return CustomerInsightsAPIAPIMPublic(credentials=auth)
 
 
 def get_custom_headers():
+    """
+    Returns the additional headers containing the API key information.
+    """
+
     api_key = GLOBAL_CONFIG.get("authentication", "api_key")
     return {
         'Cache-Control': 'no-cache',
